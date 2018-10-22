19d117d6183c0fb54557c7f3052b8b0c272feb2e
Spencer Bischof <spencerpatrick@spencerpatrick-glaptop.roam.corp.google.com>
2018-10-22 10:04:41 -0700

    Updating master

---
+++ order.py
@@ -0,0 +1,10 @@
+sentence = ("is2 Th1s T4est 3a")
+
+if len(sentence) is 0:
+    print(" ")
+
+elif len(sentence) > 0:
+    sentence2 = []
+    sentence2.append(sentence)
+    sentence2.sort()
+    print(sentence2)
